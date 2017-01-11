5c01dd2847bfa1a8236967500a18b81858cacff7
Jonas Rapp <jonas.rapp@cinteros.se>
2017-01-11 17:22:15 +0100

    Updating to most recent version number

--- FetchXmlBuilder/Properties/AssemblyInfo.cs
+++ FetchXmlBuilder/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2015.8.62")]
-[assembly: AssemblyFileVersion("1.2015.8.62")]
+[assembly: AssemblyVersion("1.2016.12.116")]
+[assembly: AssemblyFileVersion("1.2016.12.116")]
