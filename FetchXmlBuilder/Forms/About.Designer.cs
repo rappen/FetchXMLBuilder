999636aa1a44f076e9c159ec88b246e32105d93d
Jonas Rapp <jonas.rapp@innofactor.com>
2017-10-25 15:58:09 +0200

    Adding assembly version info to About box

--- FetchXmlBuilder/Forms/About.Designer.cs
+++ FetchXmlBuilder/Forms/About.Designer.cs
@@ -39,15 +39,19 @@
             this.label5 = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listAssemblies = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // linkLabel1
             // 
-            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(123, 705);
+            this.linkLabel1.Location = new System.Drawing.Point(557, 177);
             this.linkLabel1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(270, 22);
@@ -58,9 +62,8 @@
             // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(174, 598);
+            this.label2.Location = new System.Drawing.Point(558, 73);
             this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
@@ -69,9 +72,8 @@
             // 
             // linkLabel2
             // 
-            this.linkLabel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(271, 598);
+            this.linkLabel2.Location = new System.Drawing.Point(655, 73);
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(64, 13);
             this.linkLabel2.TabIndex = 4;
@@ -81,9 +83,8 @@
             // 
             // linkLabel3
             // 
-            this.linkLabel3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(271, 646);
+            this.linkLabel3.Location = new System.Drawing.Point(655, 121);
             this.linkLabel3.Name = "linkLabel3";
             this.linkLabel3.Size = new System.Drawing.Size(72, 13);
             this.linkLabel3.TabIndex = 6;
@@ -93,9 +94,8 @@
             // 
             // label3
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(174, 646);
+            this.label3.Location = new System.Drawing.Point(558, 121);
             this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
@@ -114,9 +114,8 @@
             // 
             // linkLabel4
             // 
-            this.linkLabel4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(271, 622);
+            this.linkLabel4.Location = new System.Drawing.Point(655, 97);
             this.linkLabel4.Name = "linkLabel4";
             this.linkLabel4.Size = new System.Drawing.Size(51, 13);
             this.linkLabel4.TabIndex = 9;
@@ -126,9 +125,8 @@
             // 
             // label4
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(174, 622);
+            this.label4.Location = new System.Drawing.Point(558, 97);
             this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 13);
@@ -137,9 +135,8 @@
             // 
             // label5
             // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(174, 574);
+            this.label5.Location = new System.Drawing.Point(558, 49);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 13);
             this.label5.TabIndex = 10;
@@ -147,9 +144,8 @@
             // 
             // lblVersion
             // 
-            this.lblVersion.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lblVersion.AutoSize = true;
-            this.lblVersion.Location = new System.Drawing.Point(271, 574);
+            this.lblVersion.Location = new System.Drawing.Point(655, 49);
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(22, 13);
             this.lblVersion.TabIndex = 11;
@@ -164,13 +160,50 @@
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listAssemblies);
+            this.groupBox1.Location = new System.Drawing.Point(488, 334);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 195);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // listAssemblies
+            // 
+            this.listAssemblies.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listAssemblies.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listAssemblies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listAssemblies.FullRowSelect = true;
+            this.listAssemblies.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listAssemblies.Location = new System.Drawing.Point(3, 16);
+            this.listAssemblies.Name = "listAssemblies";
+            this.listAssemblies.ShowGroups = false;
+            this.listAssemblies.Size = new System.Drawing.Size(386, 176);
+            this.listAssemblies.TabIndex = 0;
+            this.listAssemblies.UseCompatibleStateImageBehavior = false;
+            this.listAssemblies.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Assembly";
+            this.columnHeader1.Width = 254;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Version";
+            this.columnHeader2.Width = 98;
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(517, 769);
+            this.ClientSize = new System.Drawing.Size(904, 556);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.label5);
@@ -188,6 +221,7 @@
             this.Name = "About";
             this.Text = "About";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,5 +239,9 @@
         private System.Windows.Forms.Label label5;
         internal System.Windows.Forms.Label lblVersion;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listAssemblies;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
--- FetchXmlBuilder/Forms/About.cs
+++ FetchXmlBuilder/Forms/About.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Cinteros.Xrm.FetchXmlBuilder.Forms
@@ -15,6 +11,44 @@ namespace Cinteros.Xrm.FetchXmlBuilder.Forms
         public About()
         {
             InitializeComponent();
+            PopulateAssemblies();
+        }
+
+        private void PopulateAssemblies()
+        {
+            var assemblies = GetReferencedAssemblies();
+            var items = assemblies.Select(a => GetListItem(a)).ToArray();
+            listAssemblies.Items.Clear();
+            listAssemblies.Items.AddRange(items);
+        }
+
+        private ListViewItem GetListItem(AssemblyName a)
+        {
+            var item = new ListViewItem(a.Name);
+            item.SubItems.Add(a.Version.ToString());
+            return item;
+        }
+
+        private List<AssemblyName> GetReferencedAssemblies()
+        {
+            var names = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                    .Where(a => !a.Name.Equals("mscorlib") && !a.Name.StartsWith("System") && !a.Name.Contains("CSharp")).ToList();
+            names.Add(Assembly.GetEntryAssembly().GetName());
+            names.Add(Assembly.GetExecutingAssembly().GetName());
+            names = names.OrderBy(a => assemblyPrioritizer(a.Name)).ToList();
+            return names;
+        }
+
+        private static string assemblyPrioritizer(string assemblyName)
+        {
+            return
+                assemblyName.Equals("XrmToolBox") ? "AAAAAAAAAAAA" :
+                assemblyName.Contains("XrmToolBox") ? "AAAAAAAAAAAB" :
+                assemblyName.Equals(Assembly.GetExecutingAssembly().GetName().Name) ? "AAAAAAAAAAAC" :
+                assemblyName.Contains("Jonas") ? "AAAAAAAAAAAD" :
+                assemblyName.Contains("Innofactor") ? "AAAAAAAAAAAE" :
+                assemblyName.Contains("Cinteros") ? "AAAAAAAAAAAF" :
+                assemblyName;
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
