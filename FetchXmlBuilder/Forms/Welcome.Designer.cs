5b0266910f09ef982d5d792b829604ce610778ac
Jonas Rapp <jonas.rapp@innofactor.com>
2018-01-04 12:55:20 +0100

    Set minimum size on welcome screen

--- FetchXmlBuilder/Forms/Welcome.Designer.cs
+++ FetchXmlBuilder/Forms/Welcome.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.llTwitter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.llTwitter.AutoSize = true;
-            this.llTwitter.Location = new System.Drawing.Point(569, 494);
+            this.llTwitter.Location = new System.Drawing.Point(569, 496);
             this.llTwitter.Name = "llTwitter";
             this.llTwitter.Size = new System.Drawing.Size(99, 13);
             this.llTwitter.TabIndex = 24;
@@ -57,7 +57,7 @@
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(526, 494);
+            this.label4.Location = new System.Drawing.Point(526, 496);
             this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 13);
@@ -79,7 +79,7 @@
             this.llWeb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.llWeb.AutoSize = true;
             this.llWeb.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.llWeb.Location = new System.Drawing.Point(569, 513);
+            this.llWeb.Location = new System.Drawing.Point(569, 515);
             this.llWeb.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.llWeb.Name = "llWeb";
             this.llWeb.Size = new System.Drawing.Size(189, 14);
@@ -98,7 +98,7 @@
             this.txtNotes.Location = new System.Drawing.Point(491, 125);
             this.txtNotes.Name = "txtNotes";
             this.txtNotes.ReadOnly = true;
-            this.txtNotes.Size = new System.Drawing.Size(388, 353);
+            this.txtNotes.Size = new System.Drawing.Size(465, 355);
             this.txtNotes.TabIndex = 28;
             this.txtNotes.Text = "";
             // 
@@ -113,7 +113,7 @@
             this.txtWelcome.Multiline = true;
             this.txtWelcome.Name = "txtWelcome";
             this.txtWelcome.ReadOnly = true;
-            this.txtWelcome.Size = new System.Drawing.Size(388, 76);
+            this.txtWelcome.Size = new System.Drawing.Size(465, 76);
             this.txtWelcome.TabIndex = 29;
             this.txtWelcome.Text = "Welcome to version\r\n{version}";
             this.txtWelcome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -122,7 +122,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(526, 513);
+            this.label1.Location = new System.Drawing.Point(526, 515);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 30;
@@ -134,7 +134,7 @@
             this.lblContinue.AutoSize = true;
             this.lblContinue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblContinue.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblContinue.Location = new System.Drawing.Point(798, 522);
+            this.lblContinue.Location = new System.Drawing.Point(875, 524);
             this.lblContinue.Name = "lblContinue";
             this.lblContinue.Padding = new System.Windows.Forms.Padding(15, 5, 15, 5);
             this.lblContinue.Size = new System.Drawing.Size(81, 25);
@@ -157,7 +157,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(904, 556);
+            this.ClientSize = new System.Drawing.Size(981, 558);
             this.Controls.Add(this.lblContinue);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtWelcome);
@@ -170,6 +170,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(880, 580);
             this.Name = "Welcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Welcome";
