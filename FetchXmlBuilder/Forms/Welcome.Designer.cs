7157924d1b1178bf353bbaa87d8baf1aed666b99
Jonas Rapp <jonas.rapp@innofactor.com>
2018-01-04 12:30:39 +0100

    Adding welcome dialog

---
+++ FetchXmlBuilder/Forms/Welcome.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Cinteros.Xrm.FetchXmlBuilder.Forms
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+            this.llTwitter = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.llWeb = new System.Windows.Forms.LinkLabel();
+            this.txtNotes = new System.Windows.Forms.RichTextBox();
+            this.txtWelcome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContinue = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // llTwitter
+            // 
+            this.llTwitter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.llTwitter.AutoSize = true;
+            this.llTwitter.Location = new System.Drawing.Point(569, 494);
+            this.llTwitter.Name = "llTwitter";
+            this.llTwitter.Size = new System.Drawing.Size(99, 13);
+            this.llTwitter.TabIndex = 24;
+            this.llTwitter.TabStop = true;
+            this.llTwitter.Text = "@FetchXMLBuilder";
+            this.llTwitter.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llTwitter_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 494);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Twitter:";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(-90, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Contine";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // llWeb
+            // 
+            this.llWeb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.llWeb.AutoSize = true;
+            this.llWeb.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llWeb.Location = new System.Drawing.Point(569, 513);
+            this.llWeb.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.llWeb.Name = "llWeb";
+            this.llWeb.Size = new System.Drawing.Size(189, 14);
+            this.llWeb.TabIndex = 17;
+            this.llWeb.TabStop = true;
+            this.llWeb.Text = "http://fxb.xrmtoolbox.com/";
+            this.llWeb.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llWeb_LinkClicked);
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNotes.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNotes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNotes.Location = new System.Drawing.Point(491, 125);
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.ReadOnly = true;
+            this.txtNotes.Size = new System.Drawing.Size(388, 353);
+            this.txtNotes.TabIndex = 28;
+            this.txtNotes.Text = "";
+            // 
+            // txtWelcome
+            // 
+            this.txtWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWelcome.BackColor = System.Drawing.SystemColors.Window;
+            this.txtWelcome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtWelcome.Font = new System.Drawing.Font("Norwegian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWelcome.Location = new System.Drawing.Point(491, 27);
+            this.txtWelcome.Multiline = true;
+            this.txtWelcome.Name = "txtWelcome";
+            this.txtWelcome.ReadOnly = true;
+            this.txtWelcome.Size = new System.Drawing.Size(388, 76);
+            this.txtWelcome.TabIndex = 29;
+            this.txtWelcome.Text = "Welcome to version\r\n{version}";
+            this.txtWelcome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 513);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Web:";
+            // 
+            // lblContinue
+            // 
+            this.lblContinue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblContinue.AutoSize = true;
+            this.lblContinue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblContinue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblContinue.Location = new System.Drawing.Point(798, 522);
+            this.lblContinue.Name = "lblContinue";
+            this.lblContinue.Padding = new System.Windows.Forms.Padding(15, 5, 15, 5);
+            this.lblContinue.Size = new System.Drawing.Size(81, 25);
+            this.lblContinue.TabIndex = 31;
+            this.lblContinue.Text = "Continue";
+            this.lblContinue.Click += new System.EventHandler(this.lblContinue_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(407, 502);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(904, 556);
+            this.Controls.Add(this.lblContinue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWelcome);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.llTwitter);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.llWeb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Welcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Welcome";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel llTwitter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel llWeb;
+        private System.Windows.Forms.RichTextBox txtNotes;
+        private System.Windows.Forms.TextBox txtWelcome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContinue;
+    }
+}
---
+++ FetchXmlBuilder/Forms/Welcome.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinteros.Xrm.FetchXmlBuilder.Forms
+{
+    public partial class Welcome : Form
+    {
+        public static void ShowWelcome(Control owner)
+        {
+            var notes = string.Empty;
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version;
+
+            var releaseresource = assembly.GetManifestResourceNames()
+                .Where(n => n.ToLowerInvariant().Contains(".releasenotes.") && ExtractReleaseVersions(n) <= version)
+                .OrderByDescending(n => ExtractReleaseVersions(n))
+                .FirstOrDefault();
+
+            if (releaseresource != null)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(releaseresource))
+                {
+                    if (stream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            notes = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            var welcome = new Welcome();
+            welcome.txtWelcome.Text = welcome.txtWelcome.Text.Replace("{version}", version.ToString());
+            welcome.txtNotes.Rtf = notes.ToString();
+            welcome.ShowDialog(owner);
+        }
+
+        private static Version ExtractReleaseVersions(string filename)
+        {
+            filename = filename.ToLowerInvariant();
+            if (filename.Contains(".releasenotes.") && !filename.EndsWith(".releasenotes."))
+            {
+                filename = filename.Substring(filename.IndexOf(".releasenotes.") + 14);
+            }
+            if (filename.EndsWith(".rtf"))
+            {
+                filename = filename.Substring(0, filename.IndexOf(".rtf"));
+            }
+            var version = new Version(filename);
+            return version;
+        }
+
+        private Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void llTwitter_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://twitter.com/FetchXMLBuilder");
+        }
+
+        private void llWeb_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://fxb.xrmtoolbox.com");
+        }
+
+        private void lblContinue_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
