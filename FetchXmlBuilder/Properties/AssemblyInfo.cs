8086d93ae16ca436168b1c01d48822a594c018cf
Jonas Rapp <jonas@jonasrapp.net>
2019-03-10 12:04:59 +0100

    Fixed typo in nuspec and some other places

--- FetchXmlBuilder/Properties/AssemblyInfo.cs
+++ FetchXmlBuilder/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FetchXML Builder")]
-[assembly: AssemblyDescription("Build queries for CRM, XRM, CDS and the Power Platform. Investigate data. Get code. Empower yourself to achieve more.")]
+[assembly: AssemblyDescription("Build queries for D365, CRM, XRM, CDS and the Power Platform. Investigate data. Get code. Empower yourself to achieve more.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jonas Rapp, Sweden")]
 [assembly: AssemblyProduct("FetchXML Builder")]
