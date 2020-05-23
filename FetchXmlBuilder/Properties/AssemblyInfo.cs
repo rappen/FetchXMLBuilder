3d2e1dc7873de38cf88677cdb09b164cbf0cb52e
Jonas Rapp <jonas@jonasr.app>
2020-05-23 14:13:48 +0200

    ✏ Release Notes updated

--- FetchXmlBuilder/Properties/AssemblyInfo.cs
+++ FetchXmlBuilder/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jonas Rapp, Sweden")]
 [assembly: AssemblyProduct("FetchXML Builder")]
-[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2014-2019")]
+[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2014-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2019.09.1015")]
-[assembly: AssemblyFileVersion("1.2019.09.1015")]
+[assembly: AssemblyVersion("1.2020.05.1016")]
+[assembly: AssemblyFileVersion("1.2020.05.1016")]
