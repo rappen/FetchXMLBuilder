c6b78ce77aa85b3cb9378246f35289d6d1182975
Jonas Rapp <jonas@jonasr.app>
2020-05-26 21:23:54 +0200

    🧹 Moved EntityNode class to separate file

--- FetchXmlBuilder/Controls/conditionControl.cs
+++ FetchXmlBuilder/Controls/conditionControl.cs
@@ -475,39 +475,4 @@ namespace Cinteros.Xrm.FetchXmlBuilder.Controls
 
         #endregion Private Event Handlers
     }
-
-    public class EntityNode
-    {
-        #region Public Fields
-
-        public string EntityName;
-
-        #endregion Public Fields
-
-        #region Private Fields
-
-        private string name;
-
-        #endregion Private Fields
-
-        #region Public Constructors
-
-        public EntityNode(TreeNode node)
-        {
-            EntityName = TreeNodeHelper.GetAttributeFromNode(node, "name");
-            var alias = TreeNodeHelper.GetAttributeFromNode(node, "alias");
-            name = !string.IsNullOrEmpty(alias) ? alias : EntityName;
-        }
-
-        #endregion Public Constructors
-
-        #region Public Methods
-
-        public override string ToString()
-        {
-            return name;
-        }
-
-        #endregion Public Methods
-    }
 }
