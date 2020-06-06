d8b1223bf9a4516a24b73cb8b5d7a48ce011d65f
Mark Carrington <MarkMpn@users.noreply.github.com>
2020-06-06 22:29:54 +0100

    Do not prevent leaving editor control when there are validation errors

--- FetchXmlBuilder/Controls/FetchXmlElementControlBase.cs
+++ FetchXmlBuilder/Controls/FetchXmlElementControlBase.cs
@@ -112,7 +112,7 @@ namespace Cinteros.Xrm.FetchXmlBuilder.Controls
 
             if (RequiresSave())
             {
-                e.Cancel = !Save(false);
+                Save(false);
             }
         }
 
