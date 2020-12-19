053f959950662de1cb6e1e9c7c974b867e9b29e3
Mark Carrington <MarkMpn@users.noreply.github.com>
2020-12-19 21:55:15 +0000

    Added more tests

--- FXBTests/OData4Tests.cs
+++ FXBTests/OData4Tests.cs
@@ -71,6 +71,187 @@ namespace FXBTests
             Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$expand=account_contacts($select=firstname)", odata);
         }
 
+        [TestMethod]
+        public void SimpleFilter()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <filter>
+                            <condition attribute='name' operator='eq' value='FXB' />
+                        </filter>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$filter=(name eq 'FXB')", odata);
+        }
+
+        [TestMethod]
+        public void NestedFilter()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <filter>
+                            <condition attribute='name' operator='eq' value='FXB' />
+                            <filter type='or'>
+                                <condition attribute='websiteurl' operator='eq' value='xrmtoolbox.com' />
+                                <condition attribute='websiteurl' operator='eq' value='fetchxmlbuilder.com' />
+                            </filter>
+                        </filter>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$filter=(name eq 'FXB' and (websiteurl eq 'xrmtoolbox.com' or websiteurl eq 'fetchxmlbuilder.com'))", odata);
+        }
+
+        [TestMethod]
+        public void Sort()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <order attribute='name' />
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$orderby=name asc", odata);
+        }
+
+        [TestMethod]
+        public void Top()
+        {
+            var fetch = @"
+                <fetch top='10'>
+                    <entity name='account'>
+                        <attribute name='name' />
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$top=10", odata);
+        }
+
+        [TestMethod]
+        public void AggregateCount()
+        {
+            var fetch = @"
+                <fetch aggregate='true'>
+                    <entity name='account'>
+                        <attribute name='name' groupby='true' alias='name' />
+                        <attribute name='accountid' aggregate='count' alias='count' />
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$apply=groupby((name),aggregate($count as count))", odata);
+        }
+
+        [TestMethod]
+        public void AggregateMax()
+        {
+            var fetch = @"
+                <fetch aggregate='true'>
+                    <entity name='account'>
+                        <attribute name='name' groupby='true' alias='name' />
+                        <attribute name='websiteurl' aggregate='max' alias='maxwebsite' />
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$apply=groupby((name),aggregate(websiteurl with max as maxwebsite))", odata);
+        }
+
+        [TestMethod]
+        public void InnerJoinParentLink()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <link-entity name='contact' from='contactid' to='primarycontactid' link-type='inner'>
+                            <attribute name='firstname' />
+                        </link-entity>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$expand=primarycontactid($select=firstname)&$filter=(primarycontactid/contactid ne null)", odata);
+        }
+
+        [TestMethod]
+        public void InnerJoinParentLinkWithFilter()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <link-entity name='contact' from='contactid' to='primarycontactid' link-type='inner'>
+                            <attribute name='firstname' />
+                            <filter>
+                                <condition attribute='firstname' operator='eq' value='Mark' />
+                            </filter>
+                        </link-entity>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$expand=primarycontactid($select=firstname)&$filter=(primarycontactid/firstname eq 'Mark')", odata);
+        }
+
+        [TestMethod]
+        public void InnerJoinChildLink()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <link-entity name='contact' from='parentcustomerid' to='accountid' link-type='inner'>
+                            <attribute name='firstname' />
+                        </link-entity>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$expand=account_contacts($select=firstname)&$filter=(account_contacts/any(o1:(o1/contactid ne null)))", odata);
+        }
+
+        [TestMethod]
+        public void InnerJoinChildLinkWithFilter()
+        {
+            var fetch = @"
+                <fetch>
+                    <entity name='account'>
+                        <attribute name='name' />
+                        <link-entity name='contact' from='parentcustomerid' to='accountid' link-type='inner'>
+                            <attribute name='firstname' />
+                            <filter>
+                                <condition attribute='firstname' operator='eq' value='Mark' />
+                            </filter>
+                        </link-entity>
+                    </entity>
+                </fetch>";
+
+            var odata = ConvertFetchToOData(fetch);
+
+            Assert.AreEqual("https://example.crm.dynamics.com/api/data/v9.0/accounts?$select=name&$expand=account_contacts($select=firstname;$filter=(firstname eq 'Mark'))&$filter=(account_contacts/any(o1:(o1/firstname eq 'Mark')))", odata);
+        }
+
         private string ConvertFetchToOData(string fetch)
         {
             var context = new XrmFakedContext();
@@ -125,6 +306,10 @@ namespace FXBTests
                     {
                         LogicalName = "name"
                     },
+                    new StringAttributeMetadata
+                    {
+                        LogicalName = "websiteurl"
+                    },
                     new LookupAttributeMetadata
                     {
                         LogicalName = "primarycontactid",
@@ -174,6 +359,7 @@ namespace FXBTests
         {
             foreach (var entity in entities)
             {
+                SetSealedProperty(entity, nameof(EntityMetadata.PrimaryIdAttribute), attributes[entity.LogicalName].OfType<UniqueIdentifierAttributeMetadata>().Single().LogicalName);
                 SetSealedProperty(entity, nameof(EntityMetadata.Attributes), attributes[entity.LogicalName]);
             }
         }
