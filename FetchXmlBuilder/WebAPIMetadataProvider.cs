623d770e87c751540c95756da96021128127e1ea
Mark Carrington <MarkMpn@users.noreply.github.com>
2021-02-18 22:50:57 +0000

    Use submodule for FetchXML to Web API conversion

---
+++ FetchXmlBuilder/WebAPIMetadataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MarkMpn.FetchXmlToWebAPI;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace Cinteros.Xrm.FetchXmlBuilder
+{
+    internal class WebAPIMetadataProvider : IMetadataProvider
+    {
+        private FetchXmlBuilder fetchXmlBuilder;
+
+        public WebAPIMetadataProvider(FetchXmlBuilder fetchXmlBuilder)
+        {
+            this.fetchXmlBuilder = fetchXmlBuilder;
+        }
+
+        public bool IsConnected => fetchXmlBuilder.Service != null;
+
+        public EntityMetadata GetEntity(string logicalName)
+        {
+            if (fetchXmlBuilder.NeedToLoadEntity(logicalName))
+            {
+                fetchXmlBuilder.LoadEntityDetails(logicalName, null, false);
+            }
+
+            if (!fetchXmlBuilder.entities.TryGetValue(logicalName, out var metadata))
+            {
+                throw new Exception($"No metadata for entity: {logicalName}");
+            }
+
+            return metadata;
+        }
+    }
+}
