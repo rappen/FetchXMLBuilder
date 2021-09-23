bc5aca60d49b33a6ebaf375c9554170f96859eb6
Jonas Rapp <jonas@jonasr.app>
2021-09-23 14:20:25 +0200

    Added MetadataControl, using MsCrmTools.MetadataBrowser.AppCode

---
+++ FetchXmlBuilder/DockControls/MetadataControl.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xrm.Sdk.Metadata;
+using MsCrmTools.MetadataBrowser.AppCode;
+using MsCrmTools.MetadataBrowser.AppCode.AttributeMd;
+
+namespace Cinteros.Xrm.FetchXmlBuilder.DockControls
+{
+    public partial class MetadataControl : WeifenLuo.WinFormsUI.Docking.DockContent
+    {
+        private FetchXmlBuilder fxb;
+
+        public MetadataControl(FetchXmlBuilder fetchXmlBuilder)
+        {
+            fxb = fetchXmlBuilder;
+            InitializeComponent();
+        }
+
+        internal void UpdateMeta(MetadataBase meta)
+        {
+            if (meta is EntityMetadata ent)
+            {
+                Text = $"Meta Entity {fxb.GetEntityDisplayName(ent.LogicalName)}";
+                propMeta.SelectedObject = new EntityMetadataInfo(ent);
+            }
+            else if (meta is AttributeMetadata att)
+            {
+                Text = $"Meta Attribute {fxb.GetAttributeDisplayName(att.EntityLogicalName, att.LogicalName)}";
+                propMeta.SelectedObject = new AttributeMetadataInfo(att);
+            }
+            else
+            {
+                Text = "Metadata";
+                propMeta.SelectedObject = null;
+            }
+        }
+    }
+}
