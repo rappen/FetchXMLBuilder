c8078c1da8f111fa27f0db051147b9a77c203512
Jonas Rapp <jonas@jonasr.app>
2021-10-02 19:03:39 +0200

    Changed Metadata to use the UserControl

--- FetchXmlBuilder/DockControls/MetadataControl.cs
+++ FetchXmlBuilder/DockControls/MetadataControl.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.Xrm.Sdk.Metadata;
-using MsCrmTools.MetadataBrowser.AppCode;
-using MsCrmTools.MetadataBrowser.AppCode.AttributeMd;
-using System.Diagnostics;
 
 namespace Cinteros.Xrm.FetchXmlBuilder.DockControls
 {
@@ -17,32 +14,7 @@ namespace Cinteros.Xrm.FetchXmlBuilder.DockControls
 
         internal void UpdateMeta(MetadataBase meta)
         {
-            if (meta is EntityMetadata ent)
-            {
-                lblInfo1Value.Text= fxb.GetEntityDisplayName(ent.LogicalName);
-                panInfo1.Visible = true;
-                panInfo2.Visible = false;
-                propMeta.SelectedObject = new EntityMetadataInfo(ent);
-            }
-            else if (meta is AttributeMetadata att)
-            {
-                lblInfo1Value.Text = fxb.GetEntityDisplayName(att.EntityLogicalName);
-                lblInfo2Value.Text = fxb.GetAttributeDisplayName(att.EntityLogicalName, att.LogicalName);
-                panInfo1.Visible = true;
-                panInfo2.Visible = true;
-                propMeta.SelectedObject = new AttributeMetadataInfo(att);
-            }
-            else
-            {
-                panInfo1.Visible = false;
-                panInfo2.Visible = false;
-                propMeta.SelectedObject = null;
-            }
-        }
-
-        private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
-        {
-            Process.Start("https://github.com/MscrmTools/MsCrmTools.MetadataBrowser/wiki");
+            metadataControl1.SetMeta(meta);
         }
     }
 }
