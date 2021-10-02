b9bf77c51010f1ffcfa0ac2926bd1c6fd1ff314b
Jonas Rapp <jonas@jonasr.app>
2021-10-02 19:02:58 +0200

    Moved Metadata control to UserControl

---
+++ FetchXmlBuilder/Controls/metadataControl.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Cinteros.Xrm.FetchXmlBuilder.Controls
+{
+    partial class metadataControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panInfo1 = new System.Windows.Forms.Panel();
+            this.lblInfo1Value = new System.Windows.Forms.Label();
+            this.lblInfo1 = new System.Windows.Forms.Label();
+            this.panInfo2 = new System.Windows.Forms.Panel();
+            this.lblInfo2Value = new System.Windows.Forms.Label();
+            this.lblInfo2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.propMeta = new System.Windows.Forms.PropertyGrid();
+            this.panInfo1.SuspendLayout();
+            this.panInfo2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // propMeta
+            // 
+            this.propMeta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propMeta.HelpVisible = false;
+            this.propMeta.LineColor = System.Drawing.SystemColors.Window;
+            this.propMeta.Location = new System.Drawing.Point(0, 53);
+            this.propMeta.Name = "propMeta";
+            this.propMeta.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            this.propMeta.SelectedObject = this.propMeta;
+            this.propMeta.Size = new System.Drawing.Size(262, 621);
+            this.propMeta.TabIndex = 0;
+            this.propMeta.ToolbarVisible = false;
+            this.propMeta.ViewBorderColor = System.Drawing.SystemColors.Window;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 674);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 28);
+            this.panel1.TabIndex = 1;
+            // 
+            // panInfo1
+            // 
+            this.panInfo1.Controls.Add(this.lblInfo1Value);
+            this.panInfo1.Controls.Add(this.lblInfo1);
+            this.panInfo1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panInfo1.Location = new System.Drawing.Point(0, 0);
+            this.panInfo1.Name = "panInfo1";
+            this.panInfo1.Size = new System.Drawing.Size(262, 28);
+            this.panInfo1.TabIndex = 2;
+            // 
+            // lblInfo1
+            // 
+            this.lblInfo1.AutoSize = true;
+            this.lblInfo1.Location = new System.Drawing.Point(24, 13);
+            this.lblInfo1.Name = "lblInfo1";
+            this.lblInfo1.Size = new System.Drawing.Size(33, 13);
+            this.lblInfo1.TabIndex = 0;
+            this.lblInfo1.Text = "Entity";
+            // 
+            // lblInfo1Value
+            // 
+            this.lblInfo1Value.AutoSize = true;
+            this.lblInfo1Value.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo1Value.Location = new System.Drawing.Point(113, 13);
+            this.lblInfo1Value.Name = "lblInfo1Value";
+            this.lblInfo1Value.Size = new System.Drawing.Size(46, 13);
+            this.lblInfo1Value.TabIndex = 1;
+            this.lblInfo1Value.Text = "account";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(24, 8);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(190, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "MscrmTools Metadata Browser Code™";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panInfo2
+            // 
+            this.panInfo2.Controls.Add(this.lblInfo2Value);
+            this.panInfo2.Controls.Add(this.lblInfo2);
+            this.panInfo2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panInfo2.Location = new System.Drawing.Point(0, 28);
+            this.panInfo2.Name = "panInfo2";
+            this.panInfo2.Size = new System.Drawing.Size(262, 17);
+            this.panInfo2.TabIndex = 3;
+            // 
+            // lblInfo2
+            // 
+            this.lblInfo2.AutoSize = true;
+            this.lblInfo2.Location = new System.Drawing.Point(24, 3);
+            this.lblInfo2.Name = "lblInfo2";
+            this.lblInfo2.Size = new System.Drawing.Size(46, 13);
+            this.lblInfo2.TabIndex = 1;
+            this.lblInfo2.Text = "Attribute";
+            // 
+            // lblInfo2Value
+            // 
+            this.lblInfo2Value.AutoSize = true;
+            this.lblInfo2Value.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo2Value.Location = new System.Drawing.Point(113, 3);
+            this.lblInfo2Value.Name = "lblInfo2Value";
+            this.lblInfo2Value.Size = new System.Drawing.Size(33, 13);
+            this.lblInfo2Value.TabIndex = 2;
+            this.lblInfo2Value.Text = "name";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(262, 8);
+            this.panel2.TabIndex = 4;
+            // 
+            // metadataControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.propMeta);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panInfo2);
+            this.Controls.Add(this.panInfo1);
+            this.Name = "metadataControl";
+            this.Size = new System.Drawing.Size(364, 638);
+            this.panInfo1.ResumeLayout(false);
+            this.panInfo1.PerformLayout();
+            this.panInfo2.ResumeLayout(false);
+            this.panInfo2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panInfo1;
+        private System.Windows.Forms.Label lblInfo1Value;
+        private System.Windows.Forms.Label lblInfo1;
+        private System.Windows.Forms.Panel panInfo2;
+        private System.Windows.Forms.Label lblInfo2Value;
+        private System.Windows.Forms.Label lblInfo2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PropertyGrid propMeta;
+    }
+}
---
+++ FetchXmlBuilder/Controls/metadataControl.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xrm.Sdk.Metadata;
+using MsCrmTools.MetadataBrowser.AppCode;
+using MsCrmTools.MetadataBrowser.AppCode.AttributeMd;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Cinteros.Xrm.FetchXmlBuilder.Controls
+{
+    public partial class metadataControl : UserControl
+    {
+        public metadataControl()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/MscrmTools/MsCrmTools.MetadataBrowser/wiki");
+        }
+
+        internal void SetMeta(MetadataBase meta)
+        {
+            if (meta is EntityMetadata ent)
+            {
+                lblInfo1Value.Text = ent.LogicalName;
+                panInfo1.Visible = true;
+                panInfo2.Visible = false;
+                propMeta.SelectedObject = new EntityMetadataInfo(ent);
+            }
+            else if (meta is AttributeMetadata att)
+            {
+                lblInfo1Value.Text = att.EntityLogicalName;
+                lblInfo2Value.Text = att.LogicalName;
+                panInfo1.Visible = true;
+                panInfo2.Visible = true;
+                propMeta.SelectedObject = new AttributeMetadataInfo(att);
+            }
+            else
+            {
+                panInfo1.Visible = false;
+                panInfo2.Visible = false;
+                propMeta.SelectedObject = null;
+            }
+        }
+    }
+}
