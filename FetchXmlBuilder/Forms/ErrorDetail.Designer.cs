3fd24ede677d63e5f87003ceacf52e8a914b77dc
Jonas Rapp <jonas@jonasr.app>
2022-01-13 15:01:33 +0100

    Adding the new dialog ErrorDetails #613

---
+++ FetchXmlBuilder/Forms/ErrorDetail.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Cinteros.Xrm.FetchXmlBuilder.Forms
+{
+    partial class ErrorDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorDetail));
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.panButton = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panInfo = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtErrorCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtException = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDetails = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panButton.SuspendLayout();
+            this.panInfo.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfo.Location = new System.Drawing.Point(72, 29);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(402, 95);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // panButton
+            // 
+            this.panButton.Controls.Add(this.btnDetails);
+            this.panButton.Controls.Add(this.btnClose);
+            this.panButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panButton.Location = new System.Drawing.Point(0, 312);
+            this.panButton.Name = "panButton";
+            this.panButton.Size = new System.Drawing.Size(503, 50);
+            this.panButton.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Location = new System.Drawing.Point(224, 15);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // panInfo
+            // 
+            this.panInfo.Controls.Add(this.pictureBox1);
+            this.panInfo.Controls.Add(this.txtInfo);
+            this.panInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panInfo.Location = new System.Drawing.Point(0, 0);
+            this.panInfo.Name = "panInfo";
+            this.panInfo.Size = new System.Drawing.Size(503, 130);
+            this.panInfo.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtMessage);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtErrorCode);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.txtException);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 130);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(503, 182);
+            this.panel3.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.BackColor = System.Drawing.SystemColors.Window;
+            this.txtMessage.Location = new System.Drawing.Point(72, 77);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(402, 99);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message";
+            // 
+            // txtErrorCode
+            // 
+            this.txtErrorCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtErrorCode.BackColor = System.Drawing.SystemColors.Window;
+            this.txtErrorCode.Location = new System.Drawing.Point(72, 45);
+            this.txtErrorCode.Name = "txtErrorCode";
+            this.txtErrorCode.ReadOnly = true;
+            this.txtErrorCode.Size = new System.Drawing.Size(402, 20);
+            this.txtErrorCode.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Error Code";
+            // 
+            // txtException
+            // 
+            this.txtException.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtException.BackColor = System.Drawing.SystemColors.Window;
+            this.txtException.Location = new System.Drawing.Point(72, 14);
+            this.txtException.Name = "txtException";
+            this.txtException.ReadOnly = true;
+            this.txtException.Size = new System.Drawing.Size(402, 20);
+            this.txtException.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exception";
+            // 
+            // btnDetails
+            // 
+            this.btnDetails.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDetails.Location = new System.Drawing.Point(15, 15);
+            this.btnDetails.Name = "btnDetails";
+            this.btnDetails.Size = new System.Drawing.Size(75, 23);
+            this.btnDetails.TabIndex = 1;
+            this.btnDetails.Text = "Details";
+            this.btnDetails.UseVisualStyleBackColor = true;
+            this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ErrorDetail
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(503, 362);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panInfo);
+            this.Controls.Add(this.panButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ErrorDetail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Error Detail";
+            this.panButton.ResumeLayout(false);
+            this.panInfo.ResumeLayout(false);
+            this.panInfo.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Panel panButton;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panInfo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtErrorCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtException;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDetails;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ FetchXmlBuilder/Forms/ErrorDetail.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace Cinteros.Xrm.FetchXmlBuilder.Forms
+{
+    public partial class ErrorDetail : Form
+    {
+        public static void ShowDialog(Control owner, Exception error, string heading = null)
+        {
+            if (error == null)
+            {
+                return;
+            }
+            var form = new ErrorDetail();
+            if (!string.IsNullOrEmpty(heading))
+            {
+                form.Text = heading;
+            }
+            form.AddErrorInfo(error);
+            form.Height = 215;
+            form.ShowDialog(owner);
+        }
+
+        public ErrorDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void AddErrorInfo(Exception error)
+        {
+            try
+            {
+                 var msg = error.Message;
+                if (error is FaultException<OrganizationServiceFault> srcexc)
+                {
+                    msg = srcexc.Message;
+                    if (srcexc.Detail is OrganizationServiceFault orgerr)
+                    {
+                        msg = orgerr.Message;
+                        if (orgerr.InnerFault != null)
+                        {
+                            msg = orgerr.InnerFault.Message;
+                        }
+                        txtErrorCode.Text = orgerr.ErrorCode.ToString();
+                    }
+                }
+                if (msg.StartsWith("System") && msg.Contains(": ") && msg.Split(':')[0].Length < 50)
+                {
+                    msg = msg.Substring(msg.IndexOf(':') + 1);
+                }
+                if (msg.Contains("MetadataCacheDetails: "))
+                {
+                    msg = msg.Substring(0, msg.IndexOf("MetadataCacheDetails"));
+                }
+                if (msg.Contains("   at "))
+                {
+                    msg = msg.Substring(0, msg.IndexOf("   at "));
+                }
+                msg = msg.Trim();
+                txtInfo.Text = msg;
+                txtException.Text = error.GetType().ToString();
+                txtMessage.Text = error.Message;
+            }
+            catch
+            {
+                txtInfo.Text = error.Message;
+            }
+        }
+
+        private void btnDetails_Click(object sender, EventArgs e)
+        {
+            if (Height < 300)
+            {
+                Height = 400;
+            }
+            else
+            {
+                Height = 215;
+            }
+        }
+    }
+}
