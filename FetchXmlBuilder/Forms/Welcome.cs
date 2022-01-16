eadfa83e91a245be337f81e9d1ffb0559bb7f087
Jonas Rapp <jonas@jonasr.app>
2022-01-16 13:11:17 +0100

    Moving all Releases Notes to https://fetchxmlbuilder.com/releases/ (#618)

--- FetchXmlBuilder/Forms/Welcome.cs
+++ FetchXmlBuilder/Forms/Welcome.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Data;
-using System.IO;
-using System.Linq;
+﻿using Cinteros.Xrm.FetchXmlBuilder.AppCode;
+using System;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -13,30 +11,18 @@ namespace Cinteros.Xrm.FetchXmlBuilder.Forms
         {
             var assembly = Assembly.GetExecutingAssembly();
             var version = assembly.GetName().Version;
-
-            var releaseresources = assembly.GetManifestResourceNames()
-                .Where(n => n.ToLowerInvariant().Contains(".releasenotes."))
-                .Select(n => new ReleaseDoc(n))
-                .Where(n => n.Version <= version)
-                .OrderByDescending(n => n.Version);
+            var verurl = $"{version.Major}-{version.Minor}-{version.Build}";
+            var url = $"https://jonasr.app/fxb/releases/{verurl}";
+            url = Utils.ProcessURL(url);
 
             var welcome = new Welcome();
-            welcome.cmbVersions.Items.AddRange(releaseresources.ToArray());
-            welcome.txtWelcome.Text = welcome.txtWelcome.Text.Replace("{version}", version.ToString());
+            welcome.webRelease.Source = new Uri(url);
             welcome.ShowDialog(owner);
         }
 
         private Welcome()
         {
             InitializeComponent();
-        }
-
-        private void Welcome_Load(object sender, EventArgs e)
-        {
-            if (cmbVersions.Items.Count > 0)
-            {
-                cmbVersions.SelectedIndex = 0;
-            }
         }
 
         private void llTwitter_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -49,11 +35,6 @@ namespace Cinteros.Xrm.FetchXmlBuilder.Forms
             FetchXmlBuilder.OpenURL("https://fetchxmlbuilder.com");
         }
 
-        private void lblContinue_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
         private void llStats_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             MessageBox.Show(@"The evolution of FetchXML Builder is based on feedback issues and anonymous statistics collected about usage.
@@ -63,56 +44,14 @@ Thank You,
 Jonas", "Anonymous statistics", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void txtNotes_LinkClicked(object sender, LinkClickedEventArgs e)
+        private void webRelease_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
         {
-            var url = e.LinkText;
-            if (url.StartsWith("#"))
-            {
-                url = "https://github.com/rappen/FetchXMLBuilder/issues/" + url.Substring(1);
-            }
-            if (url.StartsWith("@"))
-            {
-                url = "https://twitter.com/" + url.Substring(1);
-            }
-            FetchXmlBuilder.OpenURL(url);
+            lblLoading.Visible = false;
         }
 
-        private void cmbVersions_SelectedIndexChanged(object sender, EventArgs e)
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            var notes = string.Empty;
-            if (cmbVersions.SelectedItem is ReleaseDoc releaseresource)
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                using (Stream stream = assembly.GetManifestResourceStream(releaseresource.ResourceName))
-                {
-                    if (stream != null)
-                    {
-                        using (StreamReader reader = new StreamReader(stream))
-                        {
-                            notes = reader.ReadToEnd();
-                        }
-                    }
-                }
-            }
-            txtNotes.Rtf = notes.ToString();
-        }
-    }
-
-    public class ReleaseDoc
-    {
-        public string ResourceName;
-        public Version Version;
-
-        public ReleaseDoc(string resourcename)
-        {
-            ResourceName = resourcename;
-            var docver = resourcename.ToLowerInvariant().Split(new string[] { ".releasenotes." }, StringSplitOptions.None)[1].Replace(".rtf", "");
-            Version = new Version(docver);
-        }
-
-        public override string ToString()
-        {
-            return Version.ToString();
+            FetchXmlBuilder.OpenURL(webRelease.Source.ToString());
         }
     }
 }
