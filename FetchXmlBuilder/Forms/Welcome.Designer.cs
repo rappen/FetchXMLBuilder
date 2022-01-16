3a0a47da0dd74240d7a6b109b8dbd2d17caf09c5
Jonas Rapp <jonas@jonasr.app>
2022-01-16 13:09:50 +0100

    Moving all Releases Notes to https://fetchxmlbuilder.com/releases/

--- FetchXmlBuilder/Forms/Welcome.Designer.cs
+++ FetchXmlBuilder/Forms/Welcome.Designer.cs
@@ -31,18 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
             this.llTwitter = new System.Windows.Forms.LinkLabel();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.llWeb = new System.Windows.Forms.LinkLabel();
-            this.txtNotes = new System.Windows.Forms.RichTextBox();
-            this.txtWelcome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblContinue = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.llStats = new System.Windows.Forms.LinkLabel();
-            this.cmbVersions = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.webRelease = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webRelease)).BeginInit();
             this.SuspendLayout();
             // 
             // llTwitter
@@ -50,7 +49,7 @@
             this.llTwitter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.llTwitter.AutoSize = true;
             this.llTwitter.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.llTwitter.Location = new System.Drawing.Point(585, 496);
+            this.llTwitter.Location = new System.Drawing.Point(81, 576);
             this.llTwitter.Name = "llTwitter";
             this.llTwitter.Size = new System.Drawing.Size(99, 13);
             this.llTwitter.TabIndex = 24;
@@ -62,23 +61,12 @@
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(536, 496);
+            this.label4.Location = new System.Drawing.Point(23, 576);
             this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 13);
             this.label4.TabIndex = 23;
             this.label4.Text = "Twitter:";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // button1
-            // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(-90, 377);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 22;
-            this.button1.Text = "Contine";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // llWeb
             // 
@@ -86,74 +74,29 @@
             this.llWeb.AutoSize = true;
             this.llWeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.llWeb.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.llWeb.Location = new System.Drawing.Point(585, 512);
+            this.llWeb.Location = new System.Drawing.Point(81, 592);
             this.llWeb.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.llWeb.Name = "llWeb";
-            this.llWeb.Size = new System.Drawing.Size(133, 13);
+            this.llWeb.Size = new System.Drawing.Size(136, 13);
             this.llWeb.TabIndex = 17;
             this.llWeb.TabStop = true;
             this.llWeb.Text = "https://fetchxmlbuilder.com";
             this.llWeb.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llWeb_LinkClicked);
             // 
-            // txtNotes
-            // 
-            this.txtNotes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNotes.BackColor = System.Drawing.SystemColors.Window;
-            this.txtNotes.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtNotes.Location = new System.Drawing.Point(491, 168);
-            this.txtNotes.Name = "txtNotes";
-            this.txtNotes.ReadOnly = true;
-            this.txtNotes.Size = new System.Drawing.Size(428, 316);
-            this.txtNotes.TabIndex = 28;
-            this.txtNotes.Text = "";
-            this.txtNotes.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.txtNotes_LinkClicked);
-            // 
-            // txtWelcome
-            // 
-            this.txtWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtWelcome.BackColor = System.Drawing.SystemColors.Window;
-            this.txtWelcome.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtWelcome.Font = new System.Drawing.Font("Berlin Sans FB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWelcome.Location = new System.Drawing.Point(491, 27);
-            this.txtWelcome.Multiline = true;
-            this.txtWelcome.Name = "txtWelcome";
-            this.txtWelcome.ReadOnly = true;
-            this.txtWelcome.Size = new System.Drawing.Size(428, 76);
-            this.txtWelcome.TabIndex = 29;
-            this.txtWelcome.Text = "Welcome to version\r\n{version}";
-            this.txtWelcome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(545, 512);
+            this.label1.Location = new System.Drawing.Point(23, 592);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 30;
             this.label1.Text = "Web:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lblContinue
-            // 
-            this.lblContinue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblContinue.AutoSize = true;
-            this.lblContinue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblContinue.Location = new System.Drawing.Point(838, 524);
-            this.lblContinue.Name = "lblContinue";
-            this.lblContinue.Padding = new System.Windows.Forms.Padding(15, 5, 15, 5);
-            this.lblContinue.Size = new System.Drawing.Size(81, 25);
-            this.lblContinue.TabIndex = 31;
-            this.lblContinue.Text = "Continue";
-            this.lblContinue.Click += new System.EventHandler(this.lblContinue_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(55, 27);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(407, 502);
             this.pictureBox1.TabIndex = 27;
@@ -163,19 +106,18 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(526, 528);
+            this.label2.Location = new System.Drawing.Point(23, 608);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 32;
             this.label2.Text = "Statistics:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // llStats
             // 
             this.llStats.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.llStats.AutoSize = true;
             this.llStats.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.llStats.Location = new System.Drawing.Point(585, 528);
+            this.llStats.Location = new System.Drawing.Point(81, 608);
             this.llStats.Name = "llStats";
             this.llStats.Size = new System.Drawing.Size(59, 13);
             this.llStats.TabIndex = 33;
@@ -183,44 +125,75 @@
             this.llStats.Text = "Information";
             this.llStats.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llStats_LinkClicked);
             // 
-            // cmbVersions
+            // webRelease
             // 
-            this.cmbVersions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbVersions.FormattingEnabled = true;
-            this.cmbVersions.Location = new System.Drawing.Point(569, 131);
-            this.cmbVersions.Name = "cmbVersions";
-            this.cmbVersions.Size = new System.Drawing.Size(93, 21);
-            this.cmbVersions.TabIndex = 34;
-            this.cmbVersions.SelectedIndexChanged += new System.EventHandler(this.cmbVersions_SelectedIndexChanged);
+            this.webRelease.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webRelease.CreationProperties = null;
+            this.webRelease.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webRelease.Location = new System.Drawing.Point(425, 12);
+            this.webRelease.Name = "webRelease";
+            this.webRelease.Size = new System.Drawing.Size(738, 589);
+            this.webRelease.Source = new System.Uri("https://jonasr.app/fxb/releases/1-2021-10-002/", System.UriKind.Absolute);
+            this.webRelease.TabIndex = 36;
+            this.webRelease.ZoomFactor = 1D;
+            this.webRelease.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webRelease_NavigationCompleted);
             // 
-            // label3
+            // lblLoading
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(488, 134);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 13);
-            this.label3.TabIndex = 35;
-            this.label3.Text = "Release notes";
+            this.lblLoading.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Location = new System.Drawing.Point(743, 273);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(91, 13);
+            this.lblLoading.TabIndex = 37;
+            this.lblLoading.Text = "Loading release...";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Image = ((System.Drawing.Image)(resources.GetObject("linkLabel1.Image")));
+            this.linkLabel1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.linkLabel1.Location = new System.Drawing.Point(431, 612);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
+            this.linkLabel1.TabIndex = 38;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click to open in browser      ";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(1049, 607);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(114, 23);
+            this.btnClose.TabIndex = 39;
+            this.btnClose.Text = "Continue";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // Welcome
             // 
+            this.AcceptButton = this.btnClose;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(944, 558);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.cmbVersions);
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(1175, 638);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.webRelease);
             this.Controls.Add(this.llStats);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblContinue);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtWelcome);
-            this.Controls.Add(this.txtNotes);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.llTwitter);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.llWeb);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -231,8 +204,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Welcome to FetchXML Builder";
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.Welcome_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webRelease)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,15 +215,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.LinkLabel llTwitter;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel llWeb;
-        private System.Windows.Forms.RichTextBox txtNotes;
-        private System.Windows.Forms.TextBox txtWelcome;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblContinue;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel llStats;
-        private System.Windows.Forms.ComboBox cmbVersions;
-        private System.Windows.Forms.Label label3;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webRelease;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
