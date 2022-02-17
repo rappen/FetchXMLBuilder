b324c2d2e5cd976b2efd8f11f7cd7857aaaf886c
Jonas Rapp <jonas@jonasr.app>
2022-02-17 11:09:15 +0100

    Minor code format

--- FetchXmlBuilder/AppCode/CodeGeneratorBase.cs
+++ FetchXmlBuilder/AppCode/CodeGeneratorBase.cs
@@ -13,7 +13,9 @@ namespace Cinteros.Xrm.FetchXmlBuilder.AppCode
         private static bool foundFetchData(List<NameValue> data, string name)
         {
             for (var i = 0; i < data.Count; i++)
+            {
                 if (data[i].Name == name) return true;
+            }
             return false;
         }
 
@@ -22,7 +24,8 @@ namespace Cinteros.Xrm.FetchXmlBuilder.AppCode
             var nv = new NameValue { Name = name, Value = value };
             var index = 1;
             var checkName = name;
-            while (foundFetchData(data, checkName)) {
+            while (foundFetchData(data, checkName))
+            {
                 index = index + 1;
                 checkName = name + index;
             }
